04c902228b82c25f7f5921734953ecaf98959b57
AmrajKoonar <amrajk31@gmail.com>
2025-08-08 19:35:09 -0700

    Footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -26,8 +26,9 @@ export const Footer = () => {
           <Text onBackground="neutral-weak">© {currentYear} /</Text>
           <Text paddingX="4">{person.name}</Text>
           <Text onBackground="neutral-weak">
-            {/* Usage of this template requires attribution. Please don't remove the link to Once UI. */}
-            / Build your portfolio with{" "}
+            {/* Usage of this template requires attribution. Please don't remove the link to Once UI. Link is still shown below with only extra text removed.*/}
+            {/* / Build your portfolio with{" "} */}
+            /
             <SmartLink
               href="https://once-ui.com/templates/magic-portfolio"
             >
